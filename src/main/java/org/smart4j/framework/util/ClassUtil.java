6d1f7abb08e54cbe30893908c148e4b2b465aad0
1583946113@qq.com <1583946113@qq.com>
2017-11-16 08:07:20 +0800

    commit

--- src/main/java/org/smart4j/framework/util/ClassUtil.java
+++ src/main/java/org/smart4j/framework/util/ClassUtil.java
@@ -1,8 +1,16 @@
 package org.smart4j.framework.util;
 
 import org.apache.commons.lang3.ClassUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by Toby on
@@ -18,9 +26,6 @@ public final class ClassUtil {
 
     public static Class<?> loadClass(String className, boolean isInitiallized) {
         Class<?> cls;
-
-
-
         try {
             cls = Class.forName(className, isInitiallized, getClassLoader());
         } catch (ClassNotFoundException e) {
@@ -30,5 +35,56 @@ public final class ClassUtil {
         return cls;
     }
 
+    private static void doAddClass(Set<Class<?>> classSet, String className) {
+        Class<?> cls = loadClass(className, false);
+        classSet.add(cls);
+    }
 
+    private static void addClass(Set<Class<?>> classSet, String packagePath, String packageName) {
+        File[] files = new File(packagePath).listFiles(new FileFilter() {
+            public boolean accept(File file) {
+                return (file.isFile() && file.getName().endsWith(".class")) || file.isDirectory();
+            }
+        });
+        for (File file : files) {
+            String filename = file.getName();
+            if (file.isFile()) {
+                String className = filename.substring(0, filename.lastIndexOf("."));
+                if (StringUtils.isNotEmpty(packageName)) {
+                    className = packageName + "." + className;
+                }
+                doAddClass(classSet, className);
+            } else {
+                String subPackagePath = filename;
+                if (StringUtils.isNotEmpty(packagePath)) {
+                    subPackagePath = packageName + "." + subPackagePath;
+                }
+                String subPackageName = filename;
+                if (StringUtil.isNotEmpty(packageName)) {
+                    subPackageName = packageName + "." + subPackageName;
+                }
+                addClass(classSet, subPackagePath, subPackageName);
+            }
+        }
+    }
+
+    public static Set<Class<?>> getClassSet(String packageName) {
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        try {
+            Enumeration<URL> urls = getClassLoader().getResources(packageName.replace(".", "/"));
+            while (urls.hasMoreElements()) {
+                URL url = urls.nextElement();
+                if (url != null) {
+                    String protocol = url.getProtocol();
+                    if ("file".equals(protocol)) {
+
+                    }
+                }
+            }
+        } catch (Exception e) {
+            LOGGER.error("get class set failure", e);
+            throw new RuntimeException();
+        }
+        return classSet;
+    }
 }
